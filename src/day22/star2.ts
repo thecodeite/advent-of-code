dc19f94bab43cfea720125bafd1dce30f5e892b2
Sam Plews <the@codeite.com>
2022-12-23 11:11:31 +0000

    day 22

--- src/day22/star2.ts
+++ src/day22/star2.ts
@@ -21,64 +21,7 @@ const Reset = "\x1b[0m";
 0        v.#......v
 1        v......#.v
           F>>>G>>>`;
-type Rotations = "L" | "R" | "F";
-const edgeMap: Record<string, { p: Pos; rot: Rotations }> = {};
 
-const edgeLength = 4;
-
-// prettier-ignore
-addEdges([
-  [[[8,-1], [11,-1]],[[3,3], [0,3]]],
-  [[[12,3], [12,0]],[[16,8], [16,11]]],
-  [[[12,7], [12,4]],[[12,7], [15,7]]],
-  [[[7,3], [7,0]],[[7,3], [4,3]]],
-])
-
-function addEdges(edges: [[Pos, Pos], [Pos, Pos]][]) {
-  edges.forEach(e => addEdge(e));
-}
-function addEdge([[aS, aE], [bS, bE]]: [[Pos, Pos], [Pos, Pos]]) {
-  const [adx, ady] = [aE[0] - aS[0], aE[1] - aS[1]];
-  const [bdx, bdy] = [bE[0] - bS[0], bE[1] - bS[1]];
-  console.log("adx,ady:", adx, ady);
-  console.log("bdx,bdy:", bdx, bdy);
-
-  for (let p = 0; p < edgeLength; p++) {
-    const a = {
-      x: aS[0] + Math.sign(adx) * p,
-      y: aS[1] + Math.sign(ady) * p,
-    };
-
-    const b = {
-      x: bS[0] + Math.sign(bdx) * p,
-      y: bS[1] + Math.sign(bdy) * p,
-    };
-
-    console.log(a, "->", b);
-    let rotA: Rotations;
-    let rotB: Rotations;
-    if (
-      Math.sign(adx) === Math.sign(bdx) ||
-      Math.sign(ady) === Math.sign(bdy)
-    ) {
-      rotA = rotB = "F";
-    } else if (Math.sign(adx) === Math.sign(bdy)) {
-      rotA = "R"; //Math.sign(ady) === Math.sign(bdx) ? "L" : "R";
-      rotB = "L"; //Math.sign(ady) === Math.sign(bdx) ? "R" : "L";
-    } else {
-      throw "e";
-    }
-    edgeMap[`${a.x},${a.y}`] = { p: [b.x, b.y], rot: rotA };
-    edgeMap[`${b.x},${b.y}`] = { p: [a.x, a.y], rot: rotB };
-  }
-}
-
-const value: Record<Dir, number> = {
-  ">": 0,
-  v: 1,
-  "<": 2,
-  "^": 3,
-};
 const turnRight: Record<Dir, Dir> = {
   ">": "v",
   v: "<",
@@ -91,6 +34,195 @@ const turnLeft: Record<Dir, Dir> = {
   v: ">",
   "<": "v",
   "^": "<",
+};
+
+const flip: Record<Dir, Dir> = {
+  ">": "<",
+  v: "^",
+  "<": ">",
+  "^": "v",
+};
+type Rotations = "L" | "R" | "F" | "S";
+
+interface DirMap {
+  "<"?: Dir;
+  ">"?: Dir;
+  "^"?: Dir;
+  v?: Dir;
+}
+
+interface Edge {
+  p: Pos;
+  //rot: Rotations;
+  dirMap: DirMap;
+  l: string;
+}
+const edgeMap: Record<string, Edge> = {};
+
+const edgeLength = 50;
+
+// prettier-ignore
+// addEdges([
+//   [[[8,-1],  [11,-1]], [[3,3],  [0,3]],    'A'],  // A
+//   [[[12,3],  [12,0]],  [[16,8], [16,11]],  'B'],  // B
+//   [[[12,7],  [12,4]],  [[12,7], [15,7]],   'C'],  // C
+//   [[[7,3],   [4,3]],   [[7,3],  [7,0]],    'D'],  // D
+//   [[[7,8],   [7,11]],  [[7,8],  [4,8]],    'E'],  // E
+//   [[[8,12],  [11,12]], [[3,8],  [0,8]],    'F'],  // F
+//   [[[12,12], [15,12]], [[-1,7], [-1,4]],   'G'],  // G
+// ])
+
+const straightMap: DirMap = {
+  '<':'<',
+  '>':'>',
+  '^':'^',
+  'v':'v',
+}
+const flipMap: DirMap = {
+  "<": ">",
+  ">": "<",
+  "^": "v",
+  v: "^",
+};
+
+// // prettier-ignore
+// addEdges([
+//   [[[3,7],    [0,7]],    [[3,7],  [3,4]],    'A', true],  // A
+//   [[[8,4],    [11,4]],   [[8,4],  [8,7]],    'B', false],  // B
+//   [[[4,12],   [7,12]],   [[4,12], [4,15]],   'C', true],  // C
+//   [[[12,3],   [12,0]],   [[8,8],  [8,11]],   'D', false],  // D
+//   [[[-1,8],   [-1,11]],  [[3,3],  [3,0]],    'E', false],  // E
+//   [[[7,-1],   [4,-1]],   [[-1,15],[-1,12]],  'F', true],  // F
+//   [[[8,-1],   [11,-1]],  [[0,16], [3,16]],   'G', false],  // G
+// ])
+
+// 	-1)	(0	3)	(4	7)	(8	11)	(12
+//	-1	0	  49	50	99	100	149	150
+// prettier-ignore
+addEdges([
+  [[[49,99],    [0,99]],    [[49,99],  [49,50]],    'A', true],  // A
+  [[[100,50],   [149,50]],  [[100,50], [100,99]],   'B', false],  // B
+  [[[50,150],   [99,150]],  [[50,150], [50,199]],   'C', true],  // C
+  [[[150,49],   [150,0]],   [[100,100],[100,149]],  'D', false],  // D
+  [[[-1,100],   [-1,149]],  [[49,49],  [49,0]],     'E', false],  // E
+  [[[99,-1],    [50,-1]],   [[-1,199], [-1,150]],   'F', true],  // F
+  [[[100,-1],   [149,-1]],  [[0,200],  [49,200]],   'G', false],  // G
+])
+
+function addEdges(edges: [[Pos, Pos], [Pos, Pos], string, boolean][]) {
+  edges.forEach(e => addEdge(e));
+}
+function addEdge([[aS, aE], [bS, bE], l, invert]: [
+  [Pos, Pos],
+  [Pos, Pos],
+  string,
+  boolean,
+]) {
+  const [adx, ady] = [aE[0] - aS[0], aE[1] - aS[1]];
+  const [bdx, bdy] = [bE[0] - bS[0], bE[1] - bS[1]];
+  // console.log("adx,ady:", adx, ady);
+  // console.log("bdx,bdy:", bdx, bdy);
+
+  function isCo(a: Dir, b: Dir) {
+    return (
+      (a === "<" && b === ">") ||
+      (a === ">" && b === "<") ||
+      (a === "^" && b === "v") ||
+      (a === "v" && b === "^")
+    );
+  }
+
+  let dirMap: DirMap = {};
+
+  let aAlign: Dir;
+  if (adx === 0) aAlign = ady > 0 ? "v" : "^";
+  else if (ady === 0) aAlign = adx > 0 ? ">" : "<";
+  else throw "non line";
+
+  let bAlign: Dir;
+  if (bdx === 0) bAlign = bdy > 0 ? "v" : "^";
+  else if (bdy === 0) bAlign = bdx > 0 ? ">" : "<";
+  else throw "non line";
+
+  if (aAlign === bAlign) {
+    dirMap = straightMap;
+  } else if (isCo(aAlign, bAlign)) {
+    dirMap = flipMap;
+  } else {
+    dirMap = {
+      [aAlign]: flip[bAlign],
+      [bAlign]: flip[aAlign],
+    };
+  }
+
+  console.log({ l, aAlign, bAlign });
+  // throw "stop";
+
+  for (let p = 0; p < edgeLength; p++) {
+    const a = {
+      x: aS[0] + Math.sign(adx) * p,
+      y: aS[1] + Math.sign(ady) * p,
+    };
+
+    const b = {
+      x: bS[0] + Math.sign(bdx) * p,
+      y: bS[1] + Math.sign(bdy) * p,
+    };
+
+    // console.log(a, "->", b);
+    let rotA: Rotations;
+    let rotB: Rotations;
+    // if (rotDir === 0) {
+    //   rotA = rotB = "S";
+    // } else if (rotDir % 180 === 0) {
+    //   rotA = rotB = "F";
+    // } else if (rotDir === 90) {
+    //   rotA = "R"; //Math.sign(ady) === Math.sign(bdx) ? "L" : "R";
+    //   rotB = "L"; //Math.sign(ady) === Math.sign(bdx) ? "R" : "L";
+    // } else if (rotDir === 270) {
+    //   rotA = "L"; //Math.sign(ady) === Math.sign(bdx) ? "L" : "R";
+    //   rotB = "R"; //Math.sign(ady) === Math.sign(bdx) ? "R" : "L";
+    // } else {
+    //   throw "e " + rotDir;
+    // }
+
+    // if (invert) {
+    //   [rotA, rotB] = [rotB, rotA];
+    // }
+
+    const keyA = `${a.x},${a.y}`;
+    const keyB = `${b.x},${b.y}`;
+
+    // if (edgeMap[keyA]) {
+    //   edgeMap[keyA].dirMap[bAlign] = flip[aAlign];
+    // } else {
+    edgeMap[keyA] = {
+      p: [b.x, b.y],
+      dirMap,
+      l,
+    };
+    // }
+
+    // if (edgeMap[keyB]) {
+    //   edgeMap[keyB].dirMap[aAlign] = flip[bAlign];
+    // } else {
+    edgeMap[keyB] = {
+      p: [a.x, a.y],
+      dirMap,
+      l,
+    };
+    //}
+
+    //edgeMap[keyA] = { p: [b.x, b.y], rot: rotA, l };
+    //edgeMap[keyB] = { p: [a.x, a.y], rot: rotB, l };
+  }
+}
+
+const value: Record<Dir, number> = {
+  ">": 0,
+  v: 1,
+  "<": 2,
+  "^": 3,
 };
 
 const deltaMap: Record<Dir, Pos> = {
@@ -172,32 +304,45 @@ function move(
   debug: boolean,
 ): { map: Map; pos: Pos; dir: Dir } {
   let [px, py] = pos;
-  let newDir = dir;
+  let pDir = dir;
 
   const map = initialMap.map(row => [...row]);
   for (let m = 0; m < distance; m++) {
+    let newDir = pDir;
     const [dx, dy] = deltaMap[newDir];
     map[py][px] = newDir;
 
     let [nx, ny] = [px + dx, py + dy];
-
+    // console.log({ nx, ny, newDir });
     const next = map[ny]?.[nx] ?? " ";
     if (next === " ") {
-      const wrap = edgeMap[`${nx},${ny}`];
-      if (!wrap) throw `missing: ` + `${nx},${ny}`;
-      console.log("wrap:", wrap);
-      [nx, ny] = wrap.p;
-      if (wrap.rot == "F") {
-        newDir = turnRight[turnRight[newDir]];
-      } else if (wrap.rot == "R") {
-        newDir = turnRight[newDir];
-      } else if (wrap.rot == "L") {
-        newDir = turnLeft[newDir];
-      } else {
-        throw "not imp wrap.rot";
+      const key = `${nx},${ny}`;
+      const wrap = edgeMap[key];
+      if (!wrap) {
+        console.log({ key, m, newDir });
+        throw `missing: ` + key;
       }
+      console.log("wrap:", { key, wrap, newDir, pDir, px, py });
+      [nx, ny] = wrap.p;
+
+      const mapped = wrap.dirMap[newDir];
+      if (!mapped) {
+        throw `wrap ${wrap.l} cant map ${newDir}`;
+      }
+      newDir = mapped;
+      // if (wrap.rot === "S") {
+      // } else if (wrap.rot === "F") {
+      //   newDir = turnRight[turnRight[newDir]];
+      // } else if (wrap.rot == "R") {
+      //   newDir = turnRight[newDir];
+      // } else if (wrap.rot == "L") {
+      //   newDir = turnLeft[newDir];
+      // } else {
+      //   throw "not imp wrap.rot";
+      // }
 
       const [idx, idy] = deltaMap[newDir];
+      console.log({ idx, idy, newDir });
       [nx, ny] = [nx + idx, ny + idy];
     }
 
@@ -214,14 +359,15 @@ function move(
     // if (dir === "^") {
     //   if (!next || next === " " || ny < 0) ny = findBottom(nx, map);
     // }
-    // if (debug) console.log({ a: "post", nx, ny, next });
 
+    // console.log({ a: "post", nx, ny, next });
     if (map[ny][nx] === "#") break;
     px = nx;
     py = ny;
+    pDir = newDir;
   }
 
-  return { map, pos: [px, py], dir: newDir };
+  return { map, pos: [px, py], dir: pDir };
 }
 
 export function solve(input: Input) {
@@ -258,3 +404,7 @@ export function solve(input: Input) {
   const dirVal = value[dir];
   return [row, col, dirVal, row * 1000 + col * 4 + dirVal];
 }
+
+/*
+  guess: 197047
+*/
