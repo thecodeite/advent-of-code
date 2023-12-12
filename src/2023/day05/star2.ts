6e3625f9c3a03c77b998d074c645a1dad63f3847
Sam Plews <the@codeite.com>
2023-12-12 21:49:36 +0000

    2023 - Day 5

--- src/2023/day05/star2.ts
+++ src/2023/day05/star2.ts
@@ -1,51 +1,65 @@
 import { Input } from "./parse";
 
 export function solve(input: Input) {
-  const { sections, seeds } = input
+  const { sections, seeds } = input;
   const mappers = sections.map(({ maps }) => {
     return (input: number) => {
-      const match = maps.find(({ fromStart, length }) =>
-        (input >= fromStart && input <= fromStart + length));
+      const match = maps.find(
+        ({ fromStart, length }) =>
+          input >= fromStart && input <= fromStart + length,
+      );
 
       const result = match ? match.toStart + (input - match.fromStart) : input;
 
-      process.stdout.write(` -> ${result}`)
+      // process.stdout.write(` -> ${result}`);
       return result;
     };
   });
 
+  const seePairs: number[][] = Array.from(
+    { length: seeds.length / 2 },
+    (_, i) => seeds.slice(i * 2, i * 2 + 2),
+  );
+  const totalSeeds = seePairs.reduce((p, [start, length]) => p + length, 0);
 
-  const seePairs: number[][] = Array.from({ length: seeds.length / 2 }, (_, i) => seeds.slice(i * 2, i * 2 + 2));
-
+  let count = 0;
   function* allSeeds() {
     for (const seedPair of seePairs) {
       const [start, length] = seedPair;
-      console.log(start, length);
       for (let i = 0; i < length; i++) {
         yield start + i;
       }
+      console.log(
+        `Completed ${start} of ${totalSeeds} seeds (${(
+          (count / totalSeeds) *
+          100
+        ).toFixed(2)}%)`,
+      );
     }
   }
 
-  // count number of entries in allSeeds
-  let count = 0;
-  for (const _ of allSeeds()) {
+  let lowest = Infinity;
+  for (const seed of allSeeds()) {
     count++;
-  }
-  console.log(count);
-
-  throw new Error('stop');
-
-  const locations = [1].map(seed => {
-
-    process.stdout.write(seed.toString());
-
     const result = mappers.reduce((p, c) => c(p), seed);
-    console.log();
-    return result;
-  });
+    if (result < lowest) {
+      lowest = result;
+    }
+  }
+  console.log({ count, lowest });
 
-  return Math.min(...locations);
+  // throw new Error("stop");
+
+  // const locations = [1].map(seed => {
+  //   process.stdout.write(seed.toString());
+
+  //   const result = mappers.reduce((p, c) => c(p), seed);
+  //   console.log();
+  //   return result;
+  // });
+
+  // return Math.min(...locations);
+  return lowest;
 }
 
-// trying: 379811651
+// trying: 27992443, Correct, Took 5:57.216
